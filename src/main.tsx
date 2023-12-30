1cb58a19c1a435b0e057a5bb063cd96e90757a67
Xenovka <wendyyyanto@gmail.com>
2023-12-30 13:13:52 +0700

    Setup dark-theme as the default theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { RouterProvider } from "react-router-dom";
 
 import NavBar from "./lib/layouts/NavBar/index.tsx";
 
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.tsx";
 
 const routes = router;
@@ -13,10 +14,12 @@ const routes = router;
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <NextUIProvider>
-            <main className="dark text-foreground bg-background p-4">
-                <NavBar />
-                <RouterProvider router={routes} />
-            </main>
+            <NextThemesProvider>
+                <main className="dark text-foreground bg-background p-4">
+                    <NavBar />
+                    <RouterProvider router={routes} />
+                </main>
+            </NextThemesProvider>
         </NextUIProvider>
     </React.StrictMode>
 );
